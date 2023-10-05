a7937b7fdc752753893d8e8fa436b9e10c254908
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-10-05 19:54:54 +0200

    fiubzdziu

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const para = document.createElement("p");
+para.textContent = "giqwvilgfweifgcbweifcbui";
+const div = document.createElement("div");
+div.classList.add(".gallery__item");
+console.log(div);
+const galleryUl = document.querySelector(".gallery");
+div.append(galleryUl);
+// const list = document.createElement("li");
+// galleryUl.append(list);
+// const link = document.createElement("a");
+// link.href = "";
+// link.classList.add(".gallery__link");
+
+// list.append(link);
+// const img = document.createElement("img");
+// img.src = "";
+// img.alt = "";
+// img.setAttribute("data-source", "");
+// link.append(img);
+
+galleryItems.forEach((i) => {
+  //   console.log(i.preview);
+  //   console.log(i.description);
+  //   console.log(i.original);
+  const list = document.createElement("li");
+  galleryUl.append(list);
+  const link = document.createElement("a");
+  link.href = i.original;
+  link.classList.add(".gallery__link");
+
+  list.append(link);
+  const img = document.createElement("img");
+  img.src = i.preview;
+  img.alt = i.description;
+  img.setAttribute("data-source", i.original);
+  link.append(img);
+});
